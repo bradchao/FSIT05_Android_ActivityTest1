7e5c6c7f7dd3cb907255f54575b4f6ac793a771c
Brad <brad@brad.tw>
2018-10-07 10:36:10 +0800

    v1

---
+++ app/src/main/java/tw/brad/activitytest1/MainActivity.java
@@ -0,0 +1,67 @@
+package tw.brad.activitytest1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+        Log.d("brad", "MainActivity()");
+    }
+
+    /*
+     * switch to page2
+     */
+    public void test1(View view){
+        Intent intent = new Intent(this, Page2Activity.class) ;
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("brad","onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("brad","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("brad","onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("brad","onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("brad","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("brad","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("brad","onDestroy");
+    }
+}
